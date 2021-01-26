0b84aa9330d4064bab7928bf87608e5c2a9a9c78
Julia Kosyrev <stella-agent@mail.ru>
2021-01-26 22:04:20 +0300

    new strings

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
-a = [1, 2, 3]
-b = [1, 2, 3]
-c = a
-print('a == b:', a == b)
-print('a is b:', a is b)
+class Point:
+    pass
 
-print('a == c:', a == c)
-print('a is c:', a is c)
-
-print(a, b, c)
-a.append(4)
-b.append(9)
-c.append(5)
-print(a, b, c)
-
-cache = {}
-
-def get_user(user_id):
-    if user_id not in cache:
-        return ...
-    return cache[user_id]
-
-u1 = get_user(7)
-u2 = get_user(7)
-new = 1
+p = Point()
+p.x = 2
+p.y = 1
+print(p, p.x, p.y)
